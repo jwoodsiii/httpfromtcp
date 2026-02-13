6ca9dd8eaa44b04e5652a73bcd7a8d143ef1673f
jwoodsiii <woods3.jessie@gmail.com>
2026-02-13 14:26:13 -0800

    encapsulate file read into func

--- main.go
+++ main.go
@@ -8,36 +8,47 @@ import (
 	"strings"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string)
+	go func() {
+		defer close(out)
+		defer f.Close()
+		var line string
+		buf := make([]byte, 8, 8)
+		for {
+			b, err := f.Read(buf)
+			if errors.Is(err, io.EOF) {
+				break
+			} else if err != nil {
+				fmt.Printf("error reading bytes from file: %v", err)
+				return
+			}
+			parts := strings.Split(string(buf[:b]), "\n")
+			if len(parts) == 1 {
+				line += parts[0]
+				continue
+			}
+			for i := range parts[:len(parts)-1] {
+				line += parts[i]
+				out <- line
+			}
+			line = ""
+			line += parts[len(parts)-1]
+		}
+		if line != "" {
+			out <- line
+		}
+	}()
+	return out
+}
+
 func main() {
 	file, err := os.Open("messages.txt")
 	if err != nil {
 		fmt.Printf("error opening file: %v", err)
 	}
-	defer file.Close()
-	buf := make([]byte, 8, 8)
-	var line string
-	for {
-		b, err := file.Read(buf)
-		if errors.Is(err, io.EOF) {
-			break
-		} else if err != nil {
-			fmt.Printf("error reading bytes from file: %v", err)
-		}
-		parts := strings.Split(string(buf[:b]), "\n")
-
-		if len(parts) == 1 {
-			line += parts[0]
-			continue
-		}
-		for i := range parts[:len(parts)-1] {
-			line += parts[i]
-			fmt.Printf("read: %s\n", line)
-		}
-		line = ""
-		line += parts[len(parts)-1]
+	words := getLinesChannel(file)
+	for l := range words {
+		fmt.Printf("read: %s\n", l)
 	}
-	if line != "" {
-		fmt.Printf("read: %s\n", line)
-	}
-
 }
