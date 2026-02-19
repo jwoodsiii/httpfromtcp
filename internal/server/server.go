b3dca6500049ca0bc2ea0415c1aaf791cffc2c4d
jwoodsiii <woods3.jessie@gmail.com>
2026-02-18 18:22:59 -0800

    adding response handling

--- internal/server/server.go
+++ internal/server/server.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net"
 	"sync/atomic"
+
+	response "github.com/jwoodsiii/httpfromtcp/internal/repsonse"
 )
 
 type Server struct {
@@ -58,10 +60,23 @@ func (s *Server) listen() {
 
 func (s *Server) handle(conn net.Conn) {
 	// handle single conn by writing output and then closing conn
-	_, err := conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 12\r\n\r\nHello World!"))
-	if err != nil {
-		fmt.Printf("error writing data to connection: %v", err)
+	// _, err := conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 12\r\n\r\nHello World!"))
+	// if err != nil {
+	// 	fmt.Printf("error writing data to connection: %v", err)
+	// }
+
+	// update to use internal/response to format responses
+	if err := response.WriteStatusLine(conn, response.StatusOK); err != nil {
+		fmt.Printf("error writing status line: %v", err)
+		return
 	}
+	headers := response.GetDefaultHeaders(0)
+
+	if err := response.WriteHeaders(conn, headers); err != nil {
+		fmt.Printf("error writing headers: %v", err)
+		return
+	}
+
 	if err := conn.Close(); err != nil {
 		return
 	}
