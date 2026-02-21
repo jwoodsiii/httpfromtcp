75a26b43f37f1f8878e6e8611957aed8cb34d955
jwoodsiii <woods3.jessie@gmail.com>
2026-02-21 10:56:49 -0800

    feat: add writer state validation to chunked body writes, hex conversion with fmt instead of strconv

--- internal/response/chunked_encoding.go
+++ internal/response/chunked_encoding.go
@@ -1,16 +1,23 @@
 package response
 
 import (
+	"fmt"
 	"strconv"
 )
 
 func (w *Writer) WriteChunkedBody(p []byte) (int, error) {
+	if w.writerStatus != WriterBody {
+		return 0, fmt.Errorf("cannot write body in state: %d", w.writerStatus)
+	}
 	datLen := len(p)
 	hexLen := strconv.FormatInt(int64(datLen), 16)
+
 	// written, err := w.writer.Write([]byte(fmt.Sprintf("%s\r\n%v\r\n", hexLen, p)))
 	// writing data using separate lines so we don't have to string convert byte data
 	totBytes := 0
 	b, err := w.writer.Write([]byte(hexLen + "\r\n"))
+	// can also do
+	// b, err := fmt.Fprintf(w.writer, "%x\r\n", chunkSize)
 	if err != nil {
 		return 0, err
 	}
